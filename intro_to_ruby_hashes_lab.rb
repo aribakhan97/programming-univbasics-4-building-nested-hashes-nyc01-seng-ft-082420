e3b26059777fac94d35a23d8c0ef1980fab341bf
Ariba Khan <akhan41@binghamton.edu>
2020-08-07 18:36:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,6 +27,8 @@ def monopoly_with_third_tier
   # complexity.
   hash = monopoly_with_second_tier
   hash[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  hash[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
+  hash[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
 end
 
 def monopoly_with_fourth_tier
