a9dcc1cc9431f157770325a6de7815ae3e963332
Ariba Khan <akhan41@binghamton.edu>
2020-08-07 18:32:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ def monopoly_with_second_tier
   # complexity.
   hash = base_hash
   hash[:railroads][:pieces] = 4
-  hash[:railroads][:rent_in_dollars] = {}
+  hash[:railroads][:rent_in_dollars] ={}
   hash[:railroads][:names]= {}
 
   hash
