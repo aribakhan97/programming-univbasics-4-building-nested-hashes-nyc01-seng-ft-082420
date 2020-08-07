c0aaefc5f487125fff34eed9b36452c7ed6aff40
Ariba Khan <akhan41@binghamton.edu>
2020-08-07 18:31:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,14 +13,8 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   hash = base_hash
-  puts hash
   hash[:railroads][:pieces] = 4
-  # hash[:railroads] = [
-  #   :pieces=>4,
-  #   :rent_in_dollars=>{},
-  #   :names=>{}
-  # ]
-  puts hash.has_key?(:pieces)
+  hash[:railroads][rent_in_dollars] = {}
 
   hash
 end
