e663087ccdee5fd66f8ae7949af5278dc03aaafe
Ariba Khan <akhan41@binghamton.edu>
2020-08-07 18:45:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -47,7 +47,6 @@ def monopoly_with_fourth_tier
 
 hash = monopoly_with_third_tier
 hash[:railroads][:names][:reading_railroad][:mortgage_value] = 100
-hash[:railroads][:names][:reading_railroad][:pennsylvania_railroad] = 200
-
+hash[:railroads][:names][:pennsylvania_railroad][:mortgage_value]= 200
 
 end
