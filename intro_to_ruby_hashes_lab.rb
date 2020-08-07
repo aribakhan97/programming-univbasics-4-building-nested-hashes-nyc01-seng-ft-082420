664605742f0a5c09d6664a1f73dee0e440dd4755
Ariba Khan <akhan41@binghamton.edu>
2020-08-07 18:46:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,4 +51,5 @@ hash[:railroads][:names][:pennsylvania_railroad][:mortgage_value]= 200
 hash[:railroads][:names][:b_and_o_railroad][:mortgage_value] = 400
 hash[:railroads][:names][:shortline_railroad][:mortgage_value] = 800
 
+hash
 end
